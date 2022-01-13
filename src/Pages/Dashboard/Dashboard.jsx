de92c2970639653e76ee0a43f110e2b564eff840
A. Belard <angebelard@Gmail.com>
2022-01-13 10:37:25 +0200

    Update Dashboard.jsx

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,18 +1,61 @@
-import React from 'react'
+import React, {useEffect, userEffect, useState} from 'react'
 import Logo from '../../Components/Logo/Logo'
 import Navbar from '../../Components/Navbar/Navbar'
 import classes from "./Dashboard.module.scss"
+import decode from "jwt-decode"
 
 export default function Dashboard() {
+    let decoded = decode(localStorage.token)
+    const [userinfo, setuserinfo] = useState({})
+    const [withdrawals, setwithdrawals] = useState([])
+    const [investments, setinvestments] = useState([])
+    const [isloading, setisloading] = useState(true)
+    const [active, setactive] = useState("dashboard")
+
+    useEffect(
+        () => {
+            
+        }
+    )
     return (
         <div className={classes.dashboard}>
             <Logo/>
-            <div>
-                <div>
-                    <i className='fa fa-home'></i>
-                </div>
-                <h3>Dashboard</h3>
-            </div>
+            {
+                active === "dashboard" ?
+                    (
+                    <>
+                        <div>
+                            <div>
+                                <i className='fa fa-home'></i>
+                            </div>
+                            <h3>Dashboard</h3>
+                        </div>
+                    </>
+                    )
+                :
+                active === "withdrawals" ?
+                    (
+                    <>
+                        <div>
+                            <div>
+                                <i className='fa fa-chevron-left'></i>
+                            </div>
+                            <h3>Earned</h3>
+                        </div>
+                    </>
+                    )
+                :
+                 (
+                <>
+                    <div>
+                        <div>
+                            <i className='fa fa-chevron-left'></i>
+                        </div>
+                        <h3>Investment</h3>
+                    </div>        
+                </>
+                 )
+            }
             <Navbar active={"dashboard"}/>
         </div>
     )
