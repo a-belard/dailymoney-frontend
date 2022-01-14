6ab57133a170ac9367613f445bc5b6f8b13726ed
A. Belard <angebelard@Gmail.com>
2022-01-14 21:48:04 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -10,31 +10,31 @@ import Landing from './Pages/Landing/Landing';
 import Login from './Pages/Login/Login';
 import Notifications from './Pages/Notifications/Notifications';
 import Signup from './Pages/Signup/Signup';
-import decode from "jwt-decode"
+// import decode from "jwt-decode"
 import Dailmoney from "./Pages/Dailymoney/Dailmoney";
 
 function App() {
-  let decoded;
-  if(localStorage.token){
-    if(window.location.pathname === "/login" || window.location.pathname.includes("register")){
-      window.location.href = "/dashboard"
-    }
-    try{
-      decoded = decode(localStorage.token)
-      if(!decoded.exp || decoded.exp * 1000 < new Date()){
-        localStorage.removeItem("token")
-        window.location.href = "/login"
-        }
-    }
-    catch(err){
-      localStorage.removeItem("token")
-      window.location.href = "/login"
-    }
-  }
-  else{
-    if(window.location.pathname !== "/login" && !window.location.pathname.includes("register") && window.location.pathname !== "/")
-      window.location.href = "/login"
-  }
+  // let decoded;
+  // if(localStorage.token){
+  //   if(window.location.pathname === "/login" || window.location.pathname.includes("register")){
+  //     window.location.href = "/dashboard"
+  //   }
+  //   try{
+  //     decoded = decode(localStorage.token)
+  //     if(!decoded.exp || decoded.exp * 1000 < new Date()){
+  //       localStorage.removeItem("token")
+  //       window.location.href = "/login"
+  //       }
+  //   }
+  //   catch(err){
+  //     localStorage.removeItem("token")
+  //     window.location.href = "/login"
+  //   }
+  // }
+  // else{
+  //   if(window.location.pathname !== "/login" && !window.location.pathname.includes("register") && window.location.pathname !== "/")
+  //     window.location.href = "/login"
+  // }
   return (
       <Routes>
         <Route element={<Landing/>} path="/"/>
