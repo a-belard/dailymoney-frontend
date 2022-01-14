507e506ab84c0ff710b856d4269e85c8d92874e0
A. Belard <angebelard@Gmail.com>
2022-01-14 15:54:11 +0200

    Update Navbar.jsx

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,28 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import classes from "./Navbar.module.scss"
 import { useNavigate } from "react-router-dom"
+import decode from "jwt-decode"
+import axios from "../../axios"
 
 export default function Navbar({active}) {
+    let id = decode(localStorage.token)._id
+    const [nOfNotifs, setnOfNotifs] = React.useState()
     let navigator = useNavigate()
     let navigate = (link) => {
         navigator("/" + link)
     }
+
+    useEffect(() => {
+        let getnotifs = () => {
+            axios.get("/notifications/" + id)
+            .then(data => {
+                let notifs = data
+                setnOfNotifs(notifs.data.filter(notif => notif.isread === false).length)
+            }) 
+        }
+        getnotifs()
+    }, [id])
+
     let nav = [
         {name: "Home", icon: "fa fa-home", active: "dashboard", href: "dashboard"},
         {name: "Invite", icon: "fa fa-share-alt", active: "invite", href: "referrals"},
@@ -20,6 +36,7 @@ export default function Navbar({active}) {
                 <div key={index} className={active === element.active && classes.active} onClick={e => navigate(element.href)}>
                     <div>
                         <i className={element.icon}></i>
+                        {nOfNotifs > 0 && <div></div>}
                         <span>{element.name}</span>
                         {active === element.active && <hr/>}
                     </div>
