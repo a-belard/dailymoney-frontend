298c27a26a096f324ae2fcf2a32995e8f3cbe470
A. Belard <angebelard@Gmail.com>
2022-01-14 18:25:13 +0200

    Create Adminnav.jsx

---
+++ src/Pages/Dailymoney/Adminnav/Adminnav.jsx
@@ -0,0 +1,38 @@
+import React, {useEffect} from 'react'
+import classes from "../../../Components/Navbar/Navbar.module.scss"
+import axios from "../../../axios"
+
+export default function Admin({active, changePage}) {
+    const [nOfNotifs, setnOfNotifs] = React.useState()
+    useEffect(() => {
+        let getnotifs = () => {
+            axios.get("/notifications")
+            .then(data => {
+                let notifs = data
+                setnOfNotifs(notifs.data.filter(notif => notif.isread === false).length)
+            }) 
+        }
+        getnotifs()
+    }, [])
+
+    let nav = [
+        {name: "Summary", icon: "fa fa-bars", active: "dashboard"},
+        {name: "Transactions", icon: "fa fa-business-time", active: "invite"},
+        {name: "Notif.", icon: "fa fa-bell", active: "notifications"},
+        {name: "Users", icon: "fa fa-users", active: "users"},      
+    ]
+    return (
+        <div className={classes.navbar}>
+            {nav && nav.map((element, index) => (
+                <div key={index} className={active === element.active && classes.active} onClick={() => changePage(element.active)}>
+                    <div>
+                        <i className={element.icon}></i>
+                        {nOfNotifs > 0 && <div></div>}
+                        <span>{element.name}</span>
+                        {active === element.active && <hr/>}
+                    </div>
+                </div>
+            ))}
+        </div>
+    )
+}
