87a78beb7b0eac46eb80a09911bf9dc5791c51c4
A. Belard <angebelard@Gmail.com>
2022-01-15 17:50:48 +0200

    Update Transactions.jsx

--- src/Pages/Dailymoney/Transactions/Transactions.jsx
+++ src/Pages/Dailymoney/Transactions/Transactions.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React, {useEffect, useState} from 'react'
+import { ClipLoader } from 'react-spinners'
+import axiosInstance from '../../../axios'
 import classes from "./Transactions.module.scss"
 
 export default function Transactions() {
+    const [isbalances, setisbalances] = useState(false)
+    const [transactions, settransactions] = useState([])
+    // const [balances, setbalances] = useState([])
+    const [isloading, setisloading] = useState(true)
+
+    useEffect(() => {
+        let getTransactions = async () => {
+            let transactions = await axiosInstance.get("/stats")
+            transactions = transactions.data.transactions
+            transactions = transactions
+                            .filter(transact => transact.approved === false)
+                            .filter(transaction => transaction.type === "deposit")
+            settransactions(transactions)
+            setisloading(false)
+        }
+        getTransactions()
+    })
     return (
         <div className={classes.transactions}>
             <div>
@@ -9,6 +28,52 @@ export default function Transactions() {
                     <i className='fa fa-hourglass-end'></i>
                 </div>
                 <h3>Transactions</h3>
+            </div>
+            <div>
+                <div></div>
+                <div>
+                    <button className={!isbalances && classes.active} onClick={() => setisbalances(false)}>INVESTMENTS</button>
+                    <button className={isbalances && classes.active} onClick={() => setisbalances(false)}>BALANCES</button>
+                </div>
+                {
+                    isloading ? 
+                    <span style={
+                        {
+                            display: "flex",
+                            justifyContent: "center",
+                            alignItems: "center",
+                            marginTop: "50px"
+                        }
+                    }>
+                        <ClipLoader size={20}/>
+                    </span>
+                    :
+                        transactions.length === 0 ?
+                        <span style={
+                            {
+                                display: "flex",
+                                justifyContent: "center",
+                                alignItems: "center",
+                                marginTop: "50px"
+                            }
+                        }>
+                            <strong>No pending investments!</strong>
+                        </span>
+                    :
+                    (
+                        transactions.map(transact => (
+                            <div className={classes.transacts}>
+                                <div>
+                                    <span>{transact.userId.username}</span>
+                                    <strong>{transact.amount}</strong>
+                                    <span>W. address: <strong>{transact.userId.walletAddress}</strong></span>
+                                    <button>COPY</button>
+                                </div>
+                                <button>APPROVE</button>
+                            </div>
+                        ))
+                    )
+                }
             </div>
         </div>
     )
