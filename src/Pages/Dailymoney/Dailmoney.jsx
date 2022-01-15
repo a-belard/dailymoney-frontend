0d4d253993b5e2781fd2123ca799aee1875798dd
A. Belard <angebelard@Gmail.com>
2022-01-15 17:50:58 +0200

    Update Dailmoney.jsx

--- src/Pages/Dailymoney/Dailmoney.jsx
+++ src/Pages/Dailymoney/Dailmoney.jsx
@@ -45,7 +45,7 @@ export default function Dailmoney() {
                 {
                     activepage === "dashboard" ?
                         (
-                            <Stats/>
+                            <Stats setactive={() => setactivepage("users")}/>
                         )
                     :
                     activepage === "invite" ?
